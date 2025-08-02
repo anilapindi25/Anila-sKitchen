1a96ddbdb0215f382f8f40e3bf28ac893ef361f7
anilapindi25 <2024eb01539@online.bits-pilani.ac.in>
2025-08-02 12:13:20 +0530

    Update:Iam modify the project

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -10,10 +10,10 @@ function Footer() {
         </div>
         <p>The only thing we are serious about is food.<br />Contact us on</p>
         <div className="footer-social">
-          <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" /></a>
-          <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Pinterest" /></a>
-          <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Twitter" /></a>
-          <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" /></a>
+          <a href="#"><img src="https://res.cloudinary.com/dsrifdfcy/image/upload/v1754116873/Frame_10_e8ocqg.png" alt="Instagram" /></a>
+          <a href="#"><img src="https://res.cloudinary.com/dsrifdfcy/image/upload/v1754116747/Vector_1_vwmtw4.png" alt="Pinterest" /></a>
+          <a href="#"><img src="https://res.cloudinary.com/dsrifdfcy/image/upload/v1754116914/Frame_11_jb3yu6.png" alt="Twitter" /></a>
+          <a href="#"><img src="https://res.cloudinary.com/dsrifdfcy/image/upload/v1754116955/Vector_2_gxlvgg.png" alt="Facebook" /></a>
         </div>
       </footer>
     </div>
